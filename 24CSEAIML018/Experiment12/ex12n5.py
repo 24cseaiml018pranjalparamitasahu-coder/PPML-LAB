3a7bc8c02770fb6ee378d36ae9009c694340a006
PRANJAL PARAMITA SAHU <24cseaiml018.pranjalparamitasahu@giet.edu>
2026-03-23 08:34:47 +0530

    Update ex12n5.py

--- 24CSEAIML018/Experiment12/ex12n5.py
+++ 24CSEAIML018/Experiment12/ex12n5.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3d
-fig=plt.figure
-ax=fig.add_subplot(111,projection='3d')
-x=[1,2,3,4,5]
-y=[5,6,7,8,9]
-z=[2,3,3,3,2]
-ax.scatter(x,y,z,color='r',label="3d points")
-ax.set_title("3d scatter plot")
-ax.set_xlabel("x-axis")
-ax.set_ylabel("y-axis")
-ax.set_zlabel("z-axis")
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d')
+x = [1,2,3,4,5]
+y = [5,6,7,8,9]
+z = [2,3,3,3,2]
+ax.scatter(x,y,z,color='r',label='3D points')
+ax.set_title("3D scatter plot")
+ax.set_xlabel('X-Axis')
+ax.set_ylabel("Y-Axis")
+ax.set_zlabel("Z-Axis")
 plt.legend()
 plt.show()
